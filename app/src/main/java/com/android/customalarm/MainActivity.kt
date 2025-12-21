f40514314db5214901b66d6f5991bc0da7e080af
noafl-o <noafloret@gmail.com>
2025-12-21 21:35:32 +0100

    feat: replace default content with AlarmsScreen in MainActivity

--- app/src/main/java/com/android/customalarm/MainActivity.kt
+++ app/src/main/java/com/android/customalarm/MainActivity.kt
@@ -4,36 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.android.customalarm.ui.alarms.AlarmsScreen
 import com.android.customalarm.ui.theme.CustomAlarmTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    setContent {
-      CustomAlarmTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
-        }
-      }
-    }
+    setContent { CustomAlarmTheme { AlarmsScreen() } }
   }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  CustomAlarmTheme { Greeting("Android") }
 }
