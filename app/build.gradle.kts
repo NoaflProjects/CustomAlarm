6237d298c73cab3bf17156f472d2838450e5a01e
noafl-o <noafloret@gmail.com>
2025-12-21 21:57:04 +0100

    feat: add kotlinx.coroutines.test dependency for coroutine testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Coroutines
+    testImplementation(libs.kotlinx.coroutines.test)
 }
