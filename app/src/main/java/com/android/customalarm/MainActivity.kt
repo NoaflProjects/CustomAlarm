dbfc4fd6909826b264aa9b681ed556b5e2fedd24
noafl-o <noafloret@gmail.com>
2025-12-22 18:08:26 +0100

    feat: update MainActivity to use CustomAlarmNavHost for navigation

--- app/src/main/java/com/android/customalarm/MainActivity.kt
+++ app/src/main/java/com/android/customalarm/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.android.customalarm.ui.alarms.AlarmsScreen
+import com.android.customalarm.ui.navigation.CustomAlarmNavHost
 import com.android.customalarm.ui.theme.CustomAlarmTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    setContent { CustomAlarmTheme { AlarmsScreen() } }
+    setContent { CustomAlarmTheme { CustomAlarmNavHost() } }
   }
 }
