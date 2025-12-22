f793b3deb5a269c5a31c793133c1a2af4f7a9bff
noafl-o <noafloret@gmail.com>
2025-12-22 18:07:00 +0100

    feat: add navigation dependencies for Compose and testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
+    androidTestImplementation(libs.androidx.navigation.testing)
 
     // ------------------- Unit tests (src/test) -------------------
     testImplementation(libs.junit)
