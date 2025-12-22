66f6d0d7302561f86229d3d064a3503fd734dbba
noafl-o <noafloret@gmail.com>
2025-12-22 21:16:49 +0100

    feat: rename AlarmsScreenTest to AlarmsScreenTests for consistency

--- app/src/androidTest/java/com/android/ui/alarms/AlarmsScreenTests.kt
+++ app/src/androidTest/java/com/android/ui/alarms/AlarmsScreenTests.kt
@@ -20,7 +20,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class AlarmsScreenTest {
+class AlarmsScreenTests {
 
   @get:Rule val composeRule = createComposeRule()
 
