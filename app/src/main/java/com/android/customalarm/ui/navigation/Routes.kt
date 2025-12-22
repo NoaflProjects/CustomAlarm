e95f465576501cf5623e7fc090039562850d810b
noafl-o <noafloret@gmail.com>
2025-12-22 18:07:23 +0100

    feat: add navigation routes for alarms and alarm setup

---
+++ app/src/main/java/com/android/customalarm/ui/navigation/Routes.kt
@@ -0,0 +1,7 @@
+package com.android.customalarm.ui.navigation
+
+/** Routes used in the Custom Alarm application for navigation. */
+object Routes {
+  const val ALARMS = "alarms"
+  const val ALARM_SETUP = "alarm_setup"
+}
