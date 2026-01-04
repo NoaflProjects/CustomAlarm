2aba3413474688f2342778a2cd438c340037f9e7
noafl-o <noafloret@gmail.com>
2026-01-04 16:40:20 +0100

    feat: switch AlarmsRepositoryProvider to use AlarmsRepositoryLocal

--- app/src/main/java/com/android/customalarm/model/alarms/AlarmsRepositoryProvider.kt
+++ app/src/main/java/com/android/customalarm/model/alarms/AlarmsRepositoryProvider.kt
@@ -2,7 +2,7 @@ package com.android.customalarm.model.alarms
 
 /** Provides a singleton instance of [AlarmsRepository] */
 object AlarmsRepositoryProvider {
-  private val _repository: AlarmsRepository by lazy { AlarmsRepositoryInMemory() }
+  private val _repository: AlarmsRepository by lazy { AlarmsRepositoryLocal() }
 
   var repository: AlarmsRepository = _repository
 }
